3ff9dde272423a722e2b48191dae2c7f03ee4fed
Nephthali Salam <snephthali@gmail.com>
2023-05-02 22:54:38 +0200

    Initial Commit

---
+++ src/main/java/com/nephthalisalam/movies/controllers/MovieController.java
@@ -0,0 +1,29 @@
+package com.nephthalisalam.movies.controllers;
+
+import com.nephthalisalam.movies.Movie;
+import com.nephthalisalam.movies.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    private ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    private ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId) {
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
+    }
+}
